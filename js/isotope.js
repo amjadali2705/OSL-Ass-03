0b5969fe5cfb126aadc6a2bb752cf2d39a509b5d
Amjad Ali <amjadmuz27@gmail.com>
2024-08-18 10:38:09 +0530

    ass-03-18: About page created.

--- js/isotope.js
+++ js/isotope.js
@@ -30,34 +30,71 @@
 // });
 
 
-(function ($, Drupal) {
-  Drupal.behaviors.customIsotopeAndFancybox = {
-    attach: function (context, settings) {
-      // Initialize Isotope
-      const iso = new Isotope('.isotope-container', {
-        itemSelector: '.portfolio-item',
-        layoutMode: 'masonry',
-        filter: '*',
-      });
+// (function ($, Drupal) {
+//   Drupal.behaviors.customIsotopeAndFancybox = {
+//     attach: function (context, settings) {
+//       // Initialize Isotope
+//       const iso = new Isotope('.isotope-container', {
+//         itemSelector: '.portfolio-item',
+//         layoutMode: 'masonry',
+//         filter: '*',
+//       });
 
-      // Filter items on click
-      const filters = $('.isotope-filters', context);
-      filters.on('click', 'button', function (event) {
-        const $this = $(this);
-        if (!$this.hasClass('filter-active')) {
-          const filterValue = $this.attr('data-filter');
-          iso.arrange({ filter: filterValue });
+//       // Filter items on click
+//       const filters = $('.isotope-filters', context);
+//       filters.on('click', 'button', function (event) {
+//         const $this = $(this);
+//         if (!$this.hasClass('filter-active')) {
+//           const filterValue = $this.attr('data-filter');
+//           iso.arrange({ filter: filterValue });
 
-          // Update active class
-          filters.find('.filter-active').removeClass('filter-active');
-          $this.addClass('filter-active');
+//           // Update active class
+//           filters.find('.filter-active').removeClass('filter-active');
+//           $this.addClass('filter-active');
+//         }
+//       });
+
+//       // Initialize Fancybox
+//       Fancybox.bind('[data-fancybox]', {
+//         // Add any Fancybox options here
+//       });
+//     }
+//   };
+// })(jQuery, Drupal);
+
+// Ensure DOM is fully loaded
+document.addEventListener('DOMContentLoaded', function () {
+
+  console.log('Isotope.js loaded');
+
+  // Initialize Isotope
+  var iso = new Isotope('.isotope-container', {
+    itemSelector: '.portfolio-item',
+    layoutMode: 'masonry',
+    filter: '*'
+  });
+
+  // Filter items on click
+  var filters = document.querySelector('.isotope-filters');
+  if (filters) {
+    filters.addEventListener('click', function (event) {
+      if (event.target.matches('.filter-button')) {
+        var filterValue = event.target.getAttribute('data-filter');
+        iso.arrange({ filter: filterValue });
+
+        // Update active class
+        var activeButton = filters.querySelector('.filter-active');
+        if (activeButton) {
+          activeButton.classList.remove('filter-active');
         }
-      });
+        event.target.classList.add('filter-active');
+      }
+    });
+  }
 
-      // Initialize Fancybox
-      Fancybox.bind('[data-fancybox]', {
-        // Add any Fancybox options here
-      });
-    }
-  };
-})(jQuery, Drupal);
+  // Initialize Fancybox
+  Fancybox.bind('[data-fancybox]', {
+    // Add any Fancybox options here
+  });
+
+});
